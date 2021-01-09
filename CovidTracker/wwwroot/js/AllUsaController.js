9332c26ed6165f6192d9f739b106599d320b50f6
Noa Rotem <noa.rotem@live.biu.ac.il>
2021-01-09 20:19:09 +0200

    add commennts

--- CovidTracker/wwwroot/js/AllUsaController.js
+++ CovidTracker/wwwroot/js/AllUsaController.js
@@ -1,5 +1,6 @@
-﻿app.controller('AllUsaController', function ($scope, $http) {
-    // all USA percents
+﻿//To query 4
+app.controller('AllUsaController', function ($scope, $http) {
+    //Calculate the truncated-mean of the percentage of patients for all  United States
     var usa_avg = 'covid/usa_avg'
     $http.get(usa_avg)
         .then(function success(response) {
@@ -7,12 +8,12 @@
             if (response.status != 200) {
                 console.log(response.statusText);
             } else {
+                //get the average and display it in the top of the screen
                 var average = angular.fromJson(response.data)[0]['average'];
                 $scope.percent = average;
                 var allCasualties = document.getElementById("all-casualties");
                 allCasualties.style.visibility = "visible";
             }
-            // keys:  average
         }, function error(response) {
             console.log(response.statusText);
         });
--- CovidTracker/wwwroot/js/MapController.js
+++ CovidTracker/wwwroot/js/MapController.js
@@ -1,37 +1,41 @@
 ﻿app.controller('MapController', function ($scope, $http) {
     var percents = {}
-
-
-    // 5
+    // To query 5
     // check percents for each country
+    //According to the percentages we received, we will color the map
     var map_coloring = 'covid/map_coloring'
     $http.get(map_coloring)
         .then(function success(response) {
-            console.log("this is a success")
             if (response.status != 200) {
                 console.log(response.statusText);
             } else {
-               
+                //show the map
                 var elementContainsMap = document.getElementById('the-map');
                 elementContainsMap.children[0].children[0].style.position = "relative";
+                //Get the percentage of patients for each country and insert to dictionary that his keys are the id of the
+                //state and the values are the percents for each country respectively
                 percents = angular.fromJson(response.data);
                 var percents_dictionary = {};
                 percents.forEach(function (item) {
                     percents_dictionary[item['state_str_id']] = parseFloat(item['percent']);
                 });
+                //Create a dictionary by which we will color the map
                 var countryColor = {}
                 for (var key of Object.keys(percents_dictionary)) {
+                    //if the percents of the patints in this country is higher than 10 - we will color the country in red
                     if (percents_dictionary[key] > 10) {
                         countryColor[key] = "#a82b48"
                     }
+                    //if the percents of the patints in this country is between 5 to 10 - we will color the country in orange
                     if (percents_dictionary[key] > 5 && percents_dictionary[key] <= 10) {
                         countryColor[key] = "#e38a0e"
                     }
+                    //if the percents of the patints in this country is lower than 5 - we will color the country in green
                     if (percents_dictionary[key] <= 5) {
                         countryColor[key] = "#7CA82B"
                     }
                 }
-                // keys: state_str_id, percent
+                
 
                 $scope.myJson = {
                     gui: {
@@ -55,6 +59,7 @@
                                         visible: true
 
                                     },
+                                    //the defult color for the diffrent states is green
                                     "background-color": "#7CA82B",
                                     "border-color": "#FFF",
                                     items: {
@@ -65,20 +70,17 @@
                         }
                     ]
                 };
+                //Paint the map according to the percents we get
                 for (var key of Object.keys(countryColor)) {
                     var color = countryColor[key];
                     $scope.myJson.shapes[0].options.style.items[key] = {
                         "background-color": color
                     }
                 }
-                //setTimeout(function () {
+                
                     var loader = document.getElementById('the-loader-beginning');
                     loader.remove();
-                //}, 500);
-                
-               // angular.element(loader).trigger('fadeout');
             }
-            //  $scope.months = response.data
         }, function error(response) {
             console.log(response.statusText);
         });
@@ -157,26 +159,24 @@
             });
 
 
-        // 2
-        // get month with greatest growth for each county
+        // To query 2
+        //get month with greatest growth for each county and display it as a table in a pop-up window
         var table_growth = document.getElementById("scrolledTable");
         table_growth.style.visibility = "hidden";
         var getCountyUrl = 'covid/county_growth' + "?state_str_id=" + state_str_id
         $http.get(getCountyUrl)
             .then(function success(response) {
-                console.log("this is a success")
                 if (response.status != 200) {
                     console.log(response.statusText);
                 } else {
-                    // keys: county, month
-                    table_growth.scrollTop=0;
+                    //initialize the scroll bar
+                    table_growth.scrollTop = 0;
+                    //Get the month with the largest increase compared to the previous month for each county
                     country_mounth = angular.fromJson(response.data);
                     $scope.counties = country_mounth;
-                    // make graph visible
+                    // make table visible
                     table_growth.style.visibility = "visible";
                 }
-
-                //  $scope.months = response.data
             }, function error(response) {
                 console.log(response.statusText);
             });
